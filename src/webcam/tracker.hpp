bc0667ac195df66dd73b776a999c75ac4c674767
lunaivory <catlily234@gmail.com>
2015-06-19 01:36:45 +0800

    remove unused

--- src/webcam/tracker.hpp
+++ src/webcam/tracker.hpp
@@ -14,13 +14,7 @@
 #include <algorithm>
 
 namespace webcam {
-  CvCapture* getCamera();
-  cv::Mat getShot(CvCapture* camera);
-  cv::Mat threshold(cv::Mat& image);
-  std::vector<cv::Vec3f> findCircles(cv::Mat &image_);
-  bool getScaledDots(Vector2D *left, Vector2D *middle, Vector2D *right);
-  bool circleSort(cv::Vec3f a, cv::Vec3f b);
-  void plotCircles(cv::Mat &image, std::vector<cv::Vec3f> &circles);
+bool getScaledDots(Vector2D *left, Vector2D *middle, Vector2D *right);
 }
 
 #endif
