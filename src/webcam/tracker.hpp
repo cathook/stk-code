441b85940ee585009a27d896531817701d0e3d23
lunaivory <catlily234@gmail.com>
2015-06-19 01:19:05 +0800

    tracker done

---
+++ src/webcam/tracker.hpp
@@ -0,0 +1,26 @@
+#ifndef WEBCAM_TRACKER_HPP
+#define WEBCAM_TRACKER_HPP
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#include "webcam/util.hpp"
+
+#include <vector>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+namespace webcam {
+  CvCapture* getCamera();
+  cv::Mat getShot(CvCapture* camera);
+  cv::Mat threshold(cv::Mat& image);
+  std::vector<cv::Vec3f> findCircles(cv::Mat &image_);
+  bool getScaledDots(Vector2D *left, Vector2D *middle, Vector2D *right);
+  bool circleSort(cv::Vec3f a, cv::Vec3f b);
+  void plotCircles(cv::Mat &image, std::vector<cv::Vec3f> &circles);
+}
+
+#endif
